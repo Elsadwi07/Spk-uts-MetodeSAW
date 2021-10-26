a4752f66943368d47eafdd920cab7840d9e48f98
Elsadwi07 <56227237+Elsadwi07@users.noreply.github.com>
2021-10-26 12:18:23 +0700

    Add files via upload

---
+++ beasiswa/page/home.php
@@ -0,0 +1,14 @@
+<body bgcolor="Pink">
+<h1><p align="center">
+<font color="purple">
+   	Selamat Datang di Sistem Pendukung Keputusan <br>
+	Penentuan Penerima Beasiswa di SMAN 1 GIRI <br>
+	Dengan Metode Simple Additive Weighting (SAW)
+</h1> 
+</b>
+</p>
+</font>
+<center>
+<img src="Mahasiswa3.jpg">
+</center>
+
